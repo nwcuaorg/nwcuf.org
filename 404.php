cd8a873c0c92d3104f1e9e3ecd187daea8f8b52d
James Pederson <james@jpederson.com>
2016-06-21 19:10:28 -0500

    Adjust copy on 404 page.

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 			<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
 
-			<p>Looking for the <a href="https://nwcuf.org/scholarships-and-grants">scholarships page</a>? Otherwise, feel free to try searching for what you need below.</p>
+			<p>Looking for the scholarships page? <a href="https://nwcuf.org/scholarships-and-grants">Click here</a>. Otherwise, feel free to try searching for what you need below.</p>
 
 			<?php print get_search_form(); ?>
 
