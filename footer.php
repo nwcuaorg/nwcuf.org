885638ba462428c6c8192a19e096a4d69c8bcce0
James Pederson <james@jpederson.com>
2017-08-30 11:21:32 -0500

    Update ID address.

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 					<a href="mailto:foundation@nwcua.org">foundation@nwcua.org</a></p>
 
 				<p><strong>Idaho Office:</strong><br>
-					2770 S. Vista Ave<br>
+					2710 Sunrise Rim Road, Suite 100<br>
 					Boise, ID 83705</p>
 
 				<p><strong>Oregon Office:</strong><br>
