9be445ed730bda4d4ff022a9309548aad4b0dc8c
James Pederson <james@jpederson.com>
2019-04-03 03:30:18 -0500

    Adjust the large titles to reverse the colors of the text and background, and remove the grey background on the intro text directly beneath it.

--- library/title.php
+++ library/title.php
@@ -6,7 +6,7 @@ function the_large_title() {
 	// get the slides
 	if ( has_cmb_value( 'large-title' ) ) {
 		?>
-		<div class="large-title bg-<?php show_cmb_value( 'large-title-color' ) ?>">
+		<div class="large-title text-<?php show_cmb_value( 'large-title-color' ) ?>">
 			<div class="wrap">
 				<table cellpadding="0" cellspacing="0" width="100%">
 					<tr>
