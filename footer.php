335cdb5b24959f2e36c19a4270f4a2391502ff3c
James Pederson <james@jpederson.com>
2019-05-16 17:44:50 -0500

    Add the social media icons to the footer, and include them in the site options sectioj.

--- footer.php
+++ footer.php
@@ -8,14 +8,16 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+
+$theme_options = get_option( 'pure_options' );
+
 ?>	
-	
 		</div>
 
 	</section>
 	
 	<footer class="footer">
-		<div class="wrap">
+		<div class="wrap group">
 			<div class="third">
 				<img src="<?php bloginfo( 'template_url' ) ?>/img/logo.png">
 				<nav role="navigation">
@@ -24,6 +26,12 @@
 						'menu_class' => 'nav-menu' ) 
 					); ?>
 				</nav>
+				<div class="social">
+					<a href="<?php print $theme_options['social-twitter'] ?>" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/social-twitter.png" alt="Visit our Twitter Page" /></a>
+					<a href="<?php print $theme_options['social-facebook'] ?>" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/social-facebook.png" alt="Visit our Facebook Page" /></a>
+					<a href="<?php print $theme_options['social-linkedin'] ?>" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/social-linkedin.png" alt="Visit our LinkedIn page." /></a>
+					<a href="<?php print $theme_options['social-youtube'] ?>" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/social-youtube.png" alt="Visit our Youtube Channel" /></a>
+				</div>
 			</div>
 			<div class="two-third">
 
