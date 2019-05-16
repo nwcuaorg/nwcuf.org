a99240a1f17e6a9d7758503619fa8f69ba7dd1e7
James Pederson <james@jpederson.com>
2019-05-16 15:53:14 -0500

    Update category listing template.

--- category.php
+++ category.php
@@ -11,48 +11,33 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+	<div class="wrap content-wide">
+		<div class="articles-listing">
+		<h1><?php printf( __( 'Category Archives: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
+		<?php
 
-			<?php if ( have_posts() ) : ?>
-
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
-
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .archive-header -->
-
-			<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					endwhile;
-					// Previous/next page navigation.
-					twentyfourteen_paging_nav();
-
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-
-				endif;
+		while ( have_posts() ) : the_post();
 			?>
-		</div><!-- #content -->
-	</section><!-- #primary -->
+			<article <?php post_class() ?>>
+				<div class="article-thumbnail">
+					<?php the_post_thumbnail(); ?>			
+				</div>
+				<div class="article-content">
+					<h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+					<?php the_excerpt(); ?>
+					<p class="quiet">Posted <?php print get_the_date(); ?> in <?php print get_the_category_list( ', ' ) ?></p>
+				</div>
+			</article>
+			<?php
+		endwhile;
+		?>
+		</div>
+		<div class="pagination">
+			<?php pagination() ?>
+		</div>
+	</div>
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
+
 get_footer();
+
