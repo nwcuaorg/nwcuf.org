9458941758dd931ced86ca6575a70b3f02204a44
James Pederson <james@jpederson.com>
2019-05-16 15:54:22 -0500

    Update archive template, just in case it gets used or needed in the future.

--- archive.php
+++ archive.php
@@ -6,43 +6,33 @@
 get_header(); 
 
 ?>
-	<div class="large-title bg-<?php show_cmb_value( 'large-title-color' ) ?>">
-		<div class="wrap">
-			<?php if ( has_cmb_value( 'large-title-icon' ) ) { ?>
-			<div class="large-title-icon bg-<?php show_cmb_value( 'large-title-color' ) ?>">
-				<img src="<?php show_cmb_value( 'large-title-icon' ) ?>">
-			</div>
-			<?php } ?>
-			<div class="large-title-text">
-				<h1><?php single_cat_title(); ?></h1>
-			</div>
+	<div class="wrap content-wide">
+		<div class="articles-listing">
+		<h1><?php single_cat_title(); ?></h1>
+		<?php
+
+		while ( have_posts() ) : the_post();
+			?>
+			<article <?php post_class() ?>>
+				<div class="article-thumbnail">
+					<?php the_post_thumbnail(); ?>			
+				</div>
+				<div class="article-content">
+					<h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+					<?php the_excerpt(); ?>
+					<p class="quiet">Posted <?php print get_the_date(); ?> in <?php print get_the_category_list( ', ' ) ?></p>
+				</div>
+			</article>
+			<?php
+		endwhile;
+		?>
+		</div>
+		<div class="pagination">
+			<?php pagination() ?>
 		</div>
 	</div>
-
-	<section id="primary" class="content-area wrap group" role="main">
-
-		<?php if ( have_posts() ) : ?>
-		<?php
-		
-			// Start the Loop.
-			while ( have_posts() ) : the_post(); 
-				?>
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				<?php the_excerpt(); ?>
-				<?php
-			endwhile;
-
-		else :
-			// If no content, include the "No posts found" template.
-			get_template_part( 'content', 'none' );
-
-		endif;
-		?>
-
-	</section><!-- #primary -->
 
 <?php
 
 get_footer();
 
-?>
