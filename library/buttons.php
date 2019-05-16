4f77adacc23795a2c05d091a904b9050bf8a1bdd
James Pederson <james@jpederson.com>
2019-05-16 12:46:11 -0500

    Add a force-download feature for for the buttons.

--- library/buttons.php
+++ library/buttons.php
@@ -4,11 +4,12 @@
 function button_shortcode( $atts, $content = null ) {
 	$a = shortcode_atts( array(
 		'url' => '',
-		'color' => 'green'
+		'color' => 'green',
+		'download' => false
 	), $atts );
 
 	if ( !empty( $a['url'] ) && !empty( $content ) ) {
-		return '<a href="' . $a['url'] . '" class="btn ' . $a['color'] . '">' . $content . '</a>';
+		return '<a href="' . $a['url'] . '" class="btn ' . $a['color'] . '"' . ( !empty( $a['download'] ) ? ' download="' . $a['download'] . '"' : '' ) . '>' . $content . '</a>';
 	}
 }
 add_shortcode( 'button', 'button_shortcode' );
