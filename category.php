dc14b4745161a976dd80e40d64293f03f9f63130
James Pederson <james@jpederson.com>
2019-05-29 18:30:52 -0500

    Adjust query in the correct way on the category pages.

--- category.php
+++ category.php
@@ -9,14 +9,24 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
+
+// ignore sticky posts using the pre_get_posts hook instead of query_posts (buuuugs)
+function category_query_adjust( $query ) {
+    if ( $query->is_home() && $query->is_main_query() ) {
+        $query->set( 'ignore_sticky_posts', true );
+    }
+}
+add_action( 'pre_get_posts', 'category_query_adjust' );
+
+
+get_header();
+
+?>
 
 	<div class="wrap content-wide">
 		<div class="articles-listing">
 		<h1><?php single_cat_title(); ?></h1><br>
 		<?php
-		query_posts('ignore_sticky_posts=1');
-
 		while ( have_posts() ) : the_post();
 			?>
 			<article <?php post_class() ?>>
