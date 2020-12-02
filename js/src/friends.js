fc06d2fc45dab9aeefcf9f5b6d52aab845053b12
James Pederson <james@jpederson.com>
2020-12-02 10:28:34 -0600

    Also check the BECU specific form for SC and just remove it as an option from the dropdown.

--- js/src/friends.js
+++ js/src/friends.js
@@ -13,7 +13,7 @@ var check_fotf = function( $ ) {
 jQuery(document).ready(function($){
 
     // if we're on fotf step 2.
-	if ( $('#gform_44').length > 0 ) {
+    if ( $('#gform_44').length > 0 ) {
 
         // add an error div
         $('#gform_44').find('.gform_fields').append("<li class='gfield'><label class='gfield_label'></label><div class='ginput_container error'>You may not join the foundation with BECU from South Carolina.</div></li>");
@@ -27,6 +27,14 @@ jQuery(document).ready(function($){
         $('#gform_44').find('#input_44_9').on( 'change', function(){
             check_fotf( $ );
         });
+
+    }
+
+    // if we're on fotf step 2 on the becu form.
+    if ( $('#gform_51').length > 0 ) {
+
+        // when the state field changes
+        $("#gform_51_4_4 select option[value='SC']").remove();
 
     }
 
