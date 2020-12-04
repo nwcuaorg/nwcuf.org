0861730576bee7d13ef87c43ba0a875b8ba5bb0c
James Pederson <james@jpederson.com>
2020-12-04 16:36:26 -0600

    Add tracking code for ActiveCampaign to footer.

--- footer.php
+++ footer.php
@@ -58,5 +58,13 @@ $theme_options = get_option( 'pure_options' );
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript">
+(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+vgo('setAccount', '252687469');
+vgo('setTrackByDefault', true);
+vgo('process');
+</script>
+
 </body>
 </html>
